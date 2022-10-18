810a142b36b924ae4a66bd1cbfdf67b6b85f89e4
Gabriel Lafrance <gl.gabriellafrance@gmail.com>
2022-10-17 22:37:06 -0400

    Derniere version clean

--- src/api/controllers/followersController.js
+++ src/api/controllers/followersController.js
@@ -29,51 +29,6 @@ exports.getFollowerByPost = async (req, res) => {
 }
 
 
-
-
-// exports.getAllFollowers = async (req, res) => {
-//     const limit = parseInt(req.query.limit) || 25
-//     const page = parseInt(req.query.page) || 1
-
-//     if (isNaN(limit) || isNaN(page)) {
-//         res.status(400).send({
-//             errorCode: 'INVALID_PARAMETERS',
-//             message: 'Params for pagination must be Interger'
-//         })
-//         return
-//     }
-//     const amtOfFollowers = await Followers.countDocuments()
-
-//     try {
-//         return Followers.find()
-//         .skip(limit * page - limit)
-//         .limit(limit)
-//         .exec((err, followers) => {
-//             if (err) {
-//                 res.status(500).send({
-//                     errorCode: "SERVER_ERROR",
-//                     message: 'An error occured while retriving followerss'
-//                 })
-//                 return
-//             } else {
-//                 res.status(200).send({
-//                     message: 'POST_RETRIEVED_SUCCESSFULLY',
-//                     followers,
-//                     totalPages: Math.ceil(amtOfFollowers / limit)
-//                 })
-//                 return
-//             }
-//         })
-//     } catch (e) {
-//         res.status(500).send({
-//             errorCode: 'SERVER_ERROR',
-//             message: 'An error occurred while retrieving posts'
-//           })
-//           return
-//     }
-// }
-
-
 exports.addFollower = async (req,res) => {
     console.log(req.body);
     try {
