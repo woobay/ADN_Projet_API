75cf7e29a5a27e054217f3a21874effce856b5a3
Francis Bolduc <f.bolduc@outlook.com>
2022-11-12 21:56:18 -0500

    faute de frappe au dernier push

--- src/api/models/post.js
+++ src/api/models/post.js
@@ -11,7 +11,7 @@ const postSchema = new mongoose.Schema({}).add({
     description: {type: String, required: true},
     city: {type: String, optional: true},
     created_at: {type: Date, required: true, default: Date.now},
-    push: {
+    created_by: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true},
