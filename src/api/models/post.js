e784c0fe00dc8984c0a3fc8fe18f5a525b9ae6ed
Francis Bolduc <f.bolduc@outlook.com>
2022-11-12 21:51:52 -0500

    modifier post scema, enlever le id_post, juste un tableau de id_user mainteant

--- src/api/models/post.js
+++ src/api/models/post.js
@@ -11,7 +11,7 @@ const postSchema = new mongoose.Schema({}).add({
     description: {type: String, required: true},
     city: {type: String, optional: true},
     created_at: {type: Date, required: true, default: Date.now},
-    created_by: {
+    push: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true},
