d1acbce1c1ad40ab1dbc00f1fbfa3ef999ebe0a7
Gabriel Lafrance <gl.gabriellafrance@gmail.com>
2022-11-23 08:14:39 -0500

    fix model

--- src/api/models/post.js
+++ src/api/models/post.js
@@ -7,13 +7,12 @@ const postSchema = new mongoose.Schema({}).add({
         }],
     resume: {type: String, required: true},
     description: {type: String, required: true},
-    city: {type: String, optional: true},
+    country: {type: String, optional: true},
     created_at: {type: Date, required: true, default: Date.now},
     created_by: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true,
-        default: '123'},
+        required: true},
     followers: [{
         username: {type: String, required: true},
         user_id: {type: String, required: true},
